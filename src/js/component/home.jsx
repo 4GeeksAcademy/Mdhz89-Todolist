7d70afb7b7d009ad1b2ecf44b774b6c90fa0b816
MDhz89 <mdjho89@gmail.com>
2024-09-11 04:02:34 +0000

    primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("")
+	const [taskList, setTaskList] = useState([])
+
+	const addTask = (task) => {
+		if (task.trim() != "") {
+
+			setTaskList(taskList.concat(task))
+		}
+		setInputValue("")
+	}
+	const removeTask = (index) => {
+		setTaskList(taskList.filter((_, i) => i !== index));
+	};
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className=" my-5 container">
+			<h1 className="text-center">TODOS</h1>
+			<input value={inputValue} onKeyDown={(e) => {
+				if (e.key == "Enter") {
+					addTask(inputValue)
+				}
+			}} onChange={(e) => setInputValue(e.target.value)} className="form-control form-control-lg" type="text" placeholder="What is your task?" aria-label=".form-control-lg example"></input>
+			<ul className="list-group">
+				{taskList.map((item,index) => {
+					return (
+						<li className="list-group-item d-flex justify-content-between align-items-center" key={index}>{item}
+						
+						<button type="button" onClick={() => removeTask(index) }  class="btn-close justify-end  " aria-label="Close"></button>
+					
+						</li>
+						
+					)
+				})}
+		
+	
+			
+			<span className="card-footer text-muted">
+				{taskList.length} items left
+			</span>
+			</ul>
+		
+
+	
 		</div>
 	);
 };
